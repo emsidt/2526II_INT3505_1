8aa76169f8f7be796106f3b85c46bf638d8518e1
emsidt <trinhbinhduong200x@gmail.com>
2026-03-12 07:20:07 +0700

    add: put, delete method

--- app.py
+++ app.py
@@ -22,6 +22,30 @@ def get_user(user_id):
             return jsonify(user)
     return jsonify({"error": "User not found"}), 404
 
+@app.route("/users/<int:user_id>", methods=["PUT"])
+def update_user(user_id):
+    data = request.get_json(silent=True)
+
+    if not data:
+        return jsonify({"error": "request body phai la json hop le"}), 400
+    if "name" not in data or "email" not in data:
+        return jsonify({"error": "khong tim thay ten hoac email"}), 400
+    for user in users:
+        if user["id"] == user_id:
+            user["name"] = data["name"]
+            user["email"] = data["email"]
+            return jsonify(user), 200
+    return jsonify({"error": "User not found"}), 404
+
+@app.route("/users/<int:user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    for user in users:
+        if user["id"] == user_id:
+            users.remove(user)
+            return "", 204
+
+    return jsonify({"error": "User not found"}), 404
+
 @app.route("/users", methods=["POST"])
 def create_user():
     data = request.get_json()
@@ -38,7 +62,6 @@ def create_user():
 
     users.append(new_user)
     return jsonify(new_user), 201
-
 
 if __name__ == "__main__":
     app.run(debug=True)
